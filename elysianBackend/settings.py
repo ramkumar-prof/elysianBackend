cfd5b1090cf99487da012f4d6508ec4ea31500f7
ramkumar-prof <ramkumarkulriya68@gmail.com>
2025-10-18 10:21:29 +0530

    mysql setup

--- elysianBackend/settings.py
+++ elysianBackend/settings.py
@@ -105,10 +105,15 @@ WSGI_APPLICATION = 'elysianBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'elysian',
+        'USER': 'elysian',
+        'PASSWORD': 'elysian#12345678',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
